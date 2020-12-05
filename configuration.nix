ec188c6798a5e7080b3744808d8bf29ce3baae0b
Narice <narice@tutanota.com>
2020-12-05 11:23:46 +0100

    removed jack

--- configuration.nix
+++ configuration.nix
@@ -113,19 +113,19 @@
 
     teamviewer.enable = true;
 
-    jack = {
-      jackd.enable = true;
-      # support ALSA only programs via ALSA JACK PCM plugin
-      alsa.enable = false;
-      # support ALSA only programs via loopback device (supports programs like Steam)
-      loopback = {
-        enable = true;
-        # buffering parameters for dmix device to work with ALSA only semi-professional sound programs
-        #dmixConfig = ''
-        #  period_size 2048
-        #'';
-      };
-    };
+    #jack = {
+    #  jackd.enable = true;
+    #  # support ALSA only programs via ALSA JACK PCM plugin
+    #  alsa.enable = false;
+    #  # support ALSA only programs via loopback device (supports programs like Steam)
+    #  loopback = {
+    #    enable = true;
+    #    # buffering parameters for dmix device to work with ALSA only semi-professional sound programs
+    #    #dmixConfig = ''
+    #    #  period_size 2048
+    #    #'';
+    #  };
+    #};
 
     gnome3 = {
       gnome-keyring.enable = true;

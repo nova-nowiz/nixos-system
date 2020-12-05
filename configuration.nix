0bbccc45b4c4de3b941e99fa761c80ba6ba11e41
Narice <narice@tutanota.com>
2020-12-05 12:13:52 +0100

    enabled fail2ban

--- configuration.nix
+++ configuration.nix
@@ -102,6 +102,24 @@
     # Enable the OpenSSH daemon.
     openssh.enable = true;
 
+    fail2ban = {
+      enable = true;
+      jails = {
+        apache-nohome-iptables = ''
+          # Block an IP address if it accesses a non-existent
+          # home directory more than 5 times in 10 minutes,
+          # since that indicates that it's scanning.
+          filter   = apache-nohome
+          action   = iptables-multiport[name=HTTP, port="http,https"]
+          logpath  = /var/log/httpd/error_log*
+          findtime = 600
+          bantime  = 600
+          maxretry = 5
+        '';
+      };
+      bantime-increment.enable = true;
+    };
+
 
     # Enable CUPS to print documents.
     printing = {

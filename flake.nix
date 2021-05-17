ff1ae1164b7328a9fe353d00c2943b3ada22a6ed
Narice <narice@tutanota.com>
2021-05-17 14:25:59 +0200

    Changing NixOS configuration to DevOS template

--- flake.nix
+++ flake.nix
@@ -1,136 +1,97 @@
 {
-  inputs = {
-    #nixos-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+  description = "A highly structured configuration database.";
 
-    #nixos-unstable-small.url = "github:NixOS/nixpkgs/nixos-unstable-small";
+  inputs =
+    {
+      nixos.url = "nixpkgs/nixos-unstable";
+      latest.url = "nixpkgs";
+      digga.url = "github:divnix/digga";
 
-    nixos-20-09.url = "github:NixOS/nixpkgs/nixos-20.09";
+      ci-agent = {
+        url = "github:hercules-ci/hercules-ci-agent";
+        inputs = { nix-darwin.follows = "darwin"; nixos-20_09.follows = "nixos"; nixos-unstable.follows = "latest"; };
+      };
+      darwin.url = "github:LnL7/nix-darwin";
+      darwin.inputs.nixpkgs.follows = "latest";
+      home.url = "github:nix-community/home-manager";
+      home.inputs.nixpkgs.follows = "nixos";
+      naersk.url = "github:nmattia/naersk";
+      naersk.inputs.nixpkgs.follows = "latest";
+      nixos-hardware.url = "github:nixos/nixos-hardware";
 
-    #nixos-20-09-small.url = "github:NixOS/nixpkgs/nixos-20.09-small";
-
-    #nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-
-    #home-manager-unstable = {
-    #  url = "github:nix-community/home-manager/master";
-    #  inputs.nixpkgs.follows = "/nixos-unstable";
-    #};
-
-    home-manager-20-09 = {
-      url = "github:nix-community/home-manager/release-20.09";
-      inputs.nixpkgs.follows = "/nixos-20-09";
+      pkgs.url = "path:./pkgs";
+      pkgs.inputs.nixpkgs.follows = "nixos";
     };
-  };
 
-  outputs = inputs:
-  let
-    os = inputs.nixos-20-09;
-    hm = inputs.home-manager-20-09;
-    pkgs = import os {
-      system = "x86_64-linux";
-      config = {
-        allowUnfreePredicate = pkg: builtins.elem (os.lib.getName pkg) [
-          # System Unfree Packages
-          #"nvidia-x11"
-          #"nvidia-settings"
-          #"nvidia-persistenced"
-          "teamviewer"
-        ];
+  outputs = inputs@{ self, pkgs, digga, nixos, ci-agent, home, nixos-hardware, nur, ... }:
+    digga.lib.mkFlake {
+      inherit self inputs;
 
-        packageOverrides = super: {
-          openrazer-daemon = super.openrazer-daemon.overrideAttrs (old: {
-            version = "2.9.0";
-            src = super.fetchFromGitHub {
-              owner = "openrazer";
-              repo = "openrazer";
-              rev = "v2.9.0";
-              sha256 = "1js7hq7zx5kj99brffrfaaah283ydkffmmrzsxv4mkd3nnd6rykk";
-            };
+      channelsConfig = { allowUnfree = true; };
+
+      channels = {
+        nixos = {
+          imports = [ (digga.lib.importers.overlays ./overlays) ];
+          overlays = [
+            ./pkgs/default.nix
+            pkgs.overlay # for `srcs`
+            nur.overlay
+          ];
+        };
+        latest = { };
+      };
+
+      lib = import ./lib { lib = digga.lib // nixos.lib; };
+
+      sharedOverlays = [
+        (final: prev: {
+          lib = prev.lib.extend (lfinal: lprev: {
+            our = self.lib;
           });
-          picom = super.picom.overrideAttrs (old: {
-            version = "ibhagwan";
-            src = super.fetchFromGitHub {
-              owner = "ibhagwan";
-              repo = "picom";
-              rev = "60eb00ce1b52aee46d343481d0530d5013ab850b";
-              sha256 = "sha256-PDQnWB6Gkc/FHNq0L9VX2VBcZAE++jB8NkoLQqH9J9Q=";
-            };
-          });
-        };
-      };
-    };
-  in
-  {
-    nixosConfigurations = {
-      narice-pc = os.lib.nixosSystem {
+        })
+      ];
 
-        system = "x86_64-linux";
-
-        specialArgs = {
-          inherit pkgs;
+      nixos = {
+        hostDefaults = {
+          system = "x86_64-linux";
+          channelName = "nixos";
+          modules = ./modules/module-list.nix;
+          externalModules = [
+            { _module.args.ourLib = self.lib; }
+            ci-agent.nixosModules.agent-profile
+            home.nixosModules.home-manager
+            ./modules/customBuilds.nix
+          ];
         };
 
-        modules = [ 
-          ./base.nix
-
-          hm.nixosModules.home-manager
-
-          ({config, pkgs, lib, ...}:{
-
-            networking = {
-              hostName = "narice-pc";
-              interfaces.eno1.useDHCP = true;
-            };
-
-            system.configurationRevision =
-              if inputs.self ? rev
-              then inputs.self.rev
-              else throw "Refusing to build from a dirty Git tree";
-
-            nix.registry.nixpkgs.flake = os;
-
-            home-manager = {
-              users.narice = import home/narice.nix;
-              users.monasbook = import home/monasbook.nix;
-            };
-          })
-        ];
-      };
-
-      narice-hp = os.lib.nixosSystem {
-
-        system = "x86_64-linux";
-
-        specialArgs = {
-          inherit pkgs;
+        imports = [ (digga.lib.importers.hosts ./hosts) ];
+        hosts = {
+          /* set host specific properties here */
+          NixOS = { };
         };
-
-        modules = [ 
-          ./base.nix
-
-          hm.nixosModules.home-manager
-
-          ({config, pkgs, lib, ...}:{
-
-            networking = {
-              hostName = "narice-hp";
-              #interfaces.eno1.useDHCP = true;
-              wireless.enable = true;
-            };
-
-            system.configurationRevision =
-              if inputs.self ? rev
-              then inputs.self.rev
-              else throw "Refusing to build from a dirty Git tree";
-
-            nix.registry.nixpkgs.flake = os;
-
-            home-manager = {
-              useGlobalPkgs = true;
-              users.narice = import home/narice.nix;
-            };
-          })
-        ];
+        profiles = [ ./profiles ./users ];
+        suites = { profiles, users, ... }: with profiles; rec {
+          base = [ core users.nixos users.root ];
+        };
       };
-    };
-  };
+
+      home = {
+        modules = ./users/modules/module-list.nix;
+        externalModules = [ ];
+        profiles = [ ./users/profiles ];
+        suites = { profiles, ... }: with profiles; rec {
+          base = [ direnv git ];
+        };
+      };
+
+      homeConfigurations = digga.lib.mkHomeConfigurations self.nixosConfigurations;
+
+      deploy.nodes = digga.lib.mkDeployNodes self.nixosConfigurations { };
+
+      defaultTemplate = self.templates.flk;
+      templates.flk.path = ./.;
+      templates.flk.description = "flk template";
+
+    }
+  ;
 }

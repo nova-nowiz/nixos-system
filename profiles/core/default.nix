bdbf1185877cd514d689158ff9d3b5d4439761e7
Narice <narice@tutanota.com>
2021-06-17 18:04:07 +0200

    added ports for kde-connect

--- profiles/core/default.nix
+++ profiles/core/default.nix
@@ -79,6 +79,8 @@ in
     firewall = {
       allowedTCPPorts = [ ];
       allowedUDPPorts = [ ];
+      allowedTCPPortRanges = [ { from = 1714; to = 1764; } ];
+      allowedUDPPortRanges = [ { from = 1714; to = 1764; } ];
     };
   };
 
